70c16fede21761b0fe12584cd7134eb6fb2bdc85
Fartash <fartash@mindvalley.com>
2016-11-03 11:18:16 +0800

    add Downloadmanager Class

--- download_manager.py
+++ download_manager.py
@@ -1,5 +1,6 @@
 from downloader import Downloader
 from threading import Thread
+from Queue import Queue
 
 
 class DownloadWorker(Thread):
@@ -13,3 +14,24 @@ class DownloadWorker(Thread):
             url = self.queue.get()
             self.downloader.process(url)
             self.queue.task_done()
+
+class DownloadManager(object):
+    """
+    Initiate it with list of urls and then call .process
+    """
+    def __init__(self, urls):
+        self.urls = urls
+        self.queue = Queue()
+
+    def process(self):
+        # create workers
+        for x in xrange(len(self.urls)):
+            worker = DownloadWorker(self.queue)
+            worker.daemon = True
+            worker.start()
+
+        # Put the tasks into the queue
+        for url in self.urls:
+            self.queue.put(url)
+
+        self.queue.join()
