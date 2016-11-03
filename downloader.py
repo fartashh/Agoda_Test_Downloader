50e0b035bb460d0a5bf482938d6b5fc49e110b39
Fartash <fartash@mindvalley.com>
2016-11-03 11:02:19 +0800

    downloader porcess

---
+++ downloader.py
@@ -0,0 +1,47 @@
+import logging
+logging.basicConfig()
+import urllib2
+import sys
+import json
+import os
+
+
+class Downloader(object):
+    """
+    Downloader responsible to download and validate the downloaded file
+    Instantiate it and the call the process with url
+    """
+    _logger = logging.getLogger('Downloader')
+
+    def __init__(self):
+        with open('config.json') as config:
+            self.config = json.load(config)
+
+    def process(self, url):
+        self.url = url
+        self.file_name = self.url.split('/')[-1]
+        self.file_path = os.path.join(self.config['download_location'], self.file_name)
+        try:
+            open_url = urllib2.urlopen(self.url)
+            self.meta_data = open_url.info()
+            self.file_size = float(self.meta_data.getheaders("Content-Length")[0])
+            downloaded_amount = 0.0
+            block_size = 8192
+
+            print "\nStart Downloading {} <{}>".format(self.file_name, self.file_size)
+            with open(self.file_path, 'wb') as file:
+                while True:
+                    buffer = open_url.read(block_size)
+                    if not buffer:
+                        break
+                    downloaded_amount += len(buffer)
+                    file.write(buffer)
+            return self
+        except urllib2.URLError as ex:
+            self._logger.exception('connection refused; invalid URL')
+            raise ex
+        except Exception as ex:
+            self.__validate_downloaded_file()
+            self._logger(ex)
+            raise ex
+
