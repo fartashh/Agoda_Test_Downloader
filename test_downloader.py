0970e9359fd214c1560bb5817237290dabe377b8
Fartash <fartash@mindvalley.com>
2016-11-03 10:57:31 +0800

    test http protocol

--- test_downloader.py
+++ test_downloader.py
@@ -1,1 +1,19 @@
-__author__ = 'Mindvalley'
+import unittest
+from downloader import Downloader
+import os
+import json
+
+
+class DownloadManagerTestCase(unittest.TestCase):
+    def setUp(self):
+        with open('config.json') as config:
+            self.config = json.load(config)
+        self.downloader = Downloader()
+
+    def test_http_protocol(self):
+        url = "http://mirror.aarnet.edu.au/pub/squid/squid/squid-4.0.15.tar.gz"
+        file_name = url.split('/')[-1]
+        size = 5146779
+        self.downloader.process(url)
+        file_path = os.path.join(self.config['download_location'], file_name)
+        self.assertEqual(os.path.getsize(file_path), size)
